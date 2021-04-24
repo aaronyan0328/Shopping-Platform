4ec575648800aec51ec187b518f3a18074805d95
晏辰阳 <aaronyan@yanchenangdeMBP.cable.rcn.com>
2021-04-24 09:20:52 -0400

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h2>My react / firebase project</h2>
+        <h2>Shopping platform</h2>
         <Signup />
       </header>
     </div>
